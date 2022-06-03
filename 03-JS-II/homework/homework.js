e6ef79376b3abb8d6fdfa459cfedd3e519a63f06
Hernan Caballero <hernanenriquecaballero@gmail.com>
2022-06-02 21:58:48 -0300

    error en el ultimo ejercicio

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -203,16 +203,8 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-var contador = 0
-var i = 0
-  do {
-contador+1
-i+5
- }while(contador<8)
-}
 
-
-// No modificar nada debajo de esta línea
+}// No modificar nada debajo de esta línea
 // --------------------------------
 
 module.exports = {
